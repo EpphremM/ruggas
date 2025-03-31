6de6c9b62e8a096a2a4b4373709f138371c596ea
HaileabT <haileabtesfaye11@gmail.com>
2025-03-31 00:19:39 -0400

    SETUP: setup tailwindcss, lenis and rellax on the project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  plugins: ["@/plugins/rellax.client"],
+  modules: ["lenis/nuxt", "@nuxtjs/google-fonts"],
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+  googleFonts: {
+    families: {
+      Anton: true,
+    },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <LandingView />
+  <div class="w-100vw h-svh">
+    <h1 class="font-bold bg-red-500">RUGGAS</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const rellax = useNuxtApp().$rellax;
+
+onBeforeUnmount(() => {
+  useNuxtApp().$destroyRellax();
+});
+
+onMounted(() => {
+  nextTick(() => {
+    useNuxtApp().$initRellax(".rellax", {});
+  });
+});
+</script>
+
+<style></style>
